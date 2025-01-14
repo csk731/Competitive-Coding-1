baa787f591a6a0f765e3cf591ab3bf71a15afb39
Chaitanya Allu <chaitanyaa1999@gmail.com>
2025-01-13 19:47:47 -0500

    solved Competitive Coding 1

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,35 @@
+class Problem1 {
+    private static int findMissingInteger(int arr[]) {
+        if (arr == null || arr.length == 0)
+            return -1;
 
+        int n = arr.length;
+
+        if (arr[0] != 1)
+            return 1;
+
+        if (arr[n - 1] == n)
+            return n + 1;
+
+        int l = 0;
+        int h = n - 1;
+
+        while (l <= h) {
+            int mid = (l + h) / 2;
+            if (arr[mid] - mid - 1 >= 2)
+                return -1;
+            else if (mid + 1 != arr[mid]) {
+                h = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return arr[l] - 1;
+    }
+
+    public static void main(String args[]) {
+        int arr[] = new int[] { 1, 2, 3, 4, 5, 6, 7 };
+        int ans = findMissingInteger(arr);
+        System.out.println(ans);
+    }
+}
--- Problem2.java
+++ Problem2.java
@@ -1,1 +1,14 @@
+// Question:
+// A Min-Heap is a complete binary tree in which the value in each internal node is smaller than or equal to 
+// the values in the children of that node. Mapping the elements of a heap into an array is trivial: 
+// if a node is stored an index k, then its left child is stored at index 2k + 1 and its right child at index 2k + 2.
 
+
+class Problem2 {
+
+    private static void minHeap()
+    public static void main(String args[]) {
+        int arr[] = {};
+
+    }
+}
